b3bdb798b008101760fbd135779f07cdae2dcf66
Alex Badics <admin@stickman.hu>
2018-10-13 14:36:30 +0200

    extractors: filter out amendments
    They have a different structure to actual laws, and that would confuse the
    structure parser I'm about to implement.

---
+++ law_tools/extractors/hungarian_law.py
@@ -0,0 +1,15 @@
+from collections import namedtuple
+
+from . import Extractor
+from .pdf import PdfOfLines, IndentedLine, EMPTY_LINE
+from .magyar_kozlony import MagyarKozlonyLawRawText
+
+HungarianRawLaw = namedtuple('HungarianRawLaw', ['identifier', 'subject', 'body'])
+HungarianRawAmendment = namedtuple('HungarianRawAmendment', ['identifier', 'subject', 'body'])
+
+@Extractor(MagyarKozlonyLawRawText)
+def MagyarKozlonyLawClassifier(raw):
+    if raw.subject.endswith('módosításáról'):
+        yield HungarianRawAmendment(raw.identifier, raw.subject, raw.body)
+    else:
+        yield HungarianRawLaw(raw.identifier, raw.subject, raw.body)
--- quick_test.py
+++ quick_test.py
@@ -2,11 +2,11 @@
 import sys
 from law_tools.extractors.file import BinaryFile
 from law_tools.extractors.all import do_extraction
-from law_tools.extractors.magyar_kozlony import MagyarKozlonyLawRawText
+from law_tools.extractors.hungarian_law import HungarianRawLaw
 extracted = do_extraction([BinaryFile(sys.argv[1])])
 
 for e in extracted:
-    if isinstance(e, MagyarKozlonyLawRawText):
+    if isinstance(e, HungarianRawLaw):
         print("==== {} - {} =====\n".format(e.identifier, e.subject))
         for line in e.body:
             virtual_indent = ' '*int(line.indent*0.2)
