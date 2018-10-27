0639fff4b4c53813d9924d44b22eeb819047c29f
Alex Badics <admin@stickman.hu>
2018-10-27 17:36:24 +0200

    extractors/hun: don't handle amendments specially
    It was some kind of a filtering so I wouldn't have to deal with amendments
    and their weird quoted structured things, but that's been solved recently.
    Also, simple Acts almost always contain amending Articles, so this part
    didn't do much anyway.

--- law_tools/extractors/hungarian_law.py
+++ law_tools/extractors/hungarian_law.py
@@ -7,8 +7,4 @@ from .magyar_kozlony import MagyarKozlonyLawRawText
 @Extractor(MagyarKozlonyLawRawText)
 def MagyarKozlonyLawClassifier(raw):
     # TODO: assert for 10. § (2)(c) c): 'a cím utolsó szavához a „-ról”, „-ről” rag kapcsolódjon.'
-    if raw.subject.endswith('módosításáról'):
-        # TODO: yield Amendment
-        pass
-    else:
-        yield Act(raw.identifier, raw.subject, raw.body)
+    yield Act(raw.identifier, raw.subject, raw.body)
