9f42cbb4fb6ff200476c52a3f4611f67f41a3c83
Alex Badics <admin@stickman.hu>
2019-01-02 19:00:25 +0100

    output/html: switch to grid layout
    The table layout is old-school and mostly intuitive and all, but the grid
    layout:
    - sped up loading by 70%
    - reduced generated html size by 40%
    - reduced gzipped output size by 15%
    
    It may or may not have improved machine parseability.

--- hun_law/output/html.py
+++ hun_law/output/html.py
@@ -39,27 +39,14 @@ def indent_etree_element_in_place(element, level=0):
             element.tail = indent
 
 
-def generate_html_list(list_elements, element_class):
-    css_name = 'sae_' + element_class.__name__.lower()
-    table = ET.Element('table', {'class': 'list_table {}_table'.format(css_name)})
-    for key, content_element in list_elements:
-        tr = ET.SubElement(table, 'tr')
-        key_td = ET.SubElement(tr, 'td', {'class': 'list_header {}_header'.format(css_name)})
-        key_td.text = str(key)
-
-        content_td = ET.SubElement(tr, 'td', {'class': 'list_item {}_item'.format(css_name)})
-        content_td.append(content_element)
-    return table
-
-
 def generate_html_node_for_structural_element(element):
-    container = ET.Element('div', {'class': 'structural_element se_' + element.__class__.__name__.lower()})
+    container = ET.Element('div', {'class': 'se_' + element.__class__.__name__.lower()})
     if isinstance(element, Subtitle):
         container.text = element.formatted_identifier + " " + element.title
     else:
         container.text = element.formatted_identifier
         ET.SubElement(container, 'br').tail = element.title
-    return container
+    yield container
 
 
 def get_href_for_ref(ref):
@@ -131,41 +118,54 @@ def generate_text_with_ref_links(container, text, current_ref, abbreviations):
         last_a_tag.tail = text[offset:end_offset]
 
 
-def generate_html_node_for_sub_article_elements(elements, parent_ref, abbreviations):
-    processed_elements = []
-    for e in elements:
-        if isinstance(e, SubArticleElement):
-            current_ref = e.relative_reference.relative_to(parent_ref)
-            container = ET.Element('div', {'class': 'list_item_content', 'id': current_ref.relative_id_string})
-            if e.text:
-                generate_text_with_ref_links(container, e.text, current_ref, abbreviations)
+def generate_html_nodes_for_children(element, parent_ref, abbreviations):
+    for child in element.children:
+        if isinstance(child, SubArticleElement):
+            yield from generate_html_nodes_for_sub_article_element(child, parent_ref, abbreviations)
+        elif isinstance(child, QuotedBlock):
+            yield from generate_html_nodes_for_quoted_block(child, element)
+        else:
+            raise TypeError("Unknown child type {}".format(child.__class__))
+
+
+def generate_html_nodes_for_sub_article_element(e, parent_ref, abbreviations):
+    current_ref = e.relative_reference.relative_to(parent_ref)
+    element_type_as_text = e.__class__.__name__.lower()
+    id_element = ET.Element('div', {"id": current_ref.relative_id_string, 'class': '{}_id'.format(element_type_as_text)})
+    id_element.text = e.header_prefix(e.identifier)
+    yield id_element
+    if e.text:
+        container = ET.Element('div', {'class': '{}_text'.format(element_type_as_text)})
+        generate_text_with_ref_links(container, e.text, current_ref, abbreviations)
+        yield container
+    else:
+        if e.intro:
+            intro_element = ET.Element('div', {'class': '{}_text'.format(element_type_as_text)})
+            # TODO: We don't currently parse structural amendments properly
+            # They have a two-part intro, which we unfortunately merge, which looks bad.
+            matches = re.match(r"^(.*): ?(\([^\)]*\)|\[[^\]]*\])$", e.intro)
+            if matches is not None:
+                generate_text_with_ref_links(intro_element, matches.group(1), current_ref, abbreviations)
+                ET.SubElement(intro_element, 'br').tail = matches.group(2)
             else:
-                if e.intro:
-                    intro = ET.SubElement(container, 'div')
-                    # TODO: We don't currently parse structural amendments properly
-                    # They have a two-part intro, which we unfortunately merge, which looks bad.
-                    matches = re.match(r"^(.*): ?(\([^\)]*\)|\[[^\]]*\])$", e.intro)
-                    if matches is not None:
-                        generate_text_with_ref_links(intro, matches.group(1), current_ref, abbreviations)
-                        ET.SubElement(intro, 'br').tail = matches.group(2)
-                    else:
-                        generate_text_with_ref_links(intro, e.intro, current_ref, abbreviations)
-                container.append(generate_html_node_for_sub_article_elements(e.children, current_ref, abbreviations))
-                if e.wrap_up:
-                    wrap_up = ET.SubElement(container, 'div')
-                    wrap_up.text = e.wrap_up
-            header = e.header_prefix(e.identifier)
-            processed_elements.append((header, container))
-        elif isinstance(e, QuotedBlock):
-            processed_elements.append(('', generate_html_node_for_quoted_block(e)))
-    return generate_html_list(processed_elements, elements[0].__class__)
+                generate_text_with_ref_links(intro_element, e.intro, current_ref, abbreviations)
+            yield intro_element
+
+        yield from generate_html_nodes_for_children(e, current_ref, abbreviations)
+
+        if e.wrap_up:
+            wrap_up_element = ET.Element('div', {'class': '{}_text'.format(element_type_as_text)})
+            wrap_up_element.text = e.wrap_up
+            yield wrap_up_element
 
 
-def generate_html_node_for_quoted_block(element):
-    container = ET.Element('blockquote')
+def generate_html_nodes_for_quoted_block(element, parent):
+    container = ET.Element('blockquote', {'class': 'quote_in_{}'.format(parent.__class__.__name__.lower())})
     indent_offset = min(l.indent for l in element.lines if l != EMPTY_LINE)
     for index, l in enumerate(element.lines):
         padding = int((l.indent-indent_offset) * 2)
+        if padding < 0:
+            padding = 0
         linediv = ET.SubElement(container, 'div', {'style': 'padding-left: {}px;'.format(padding)})
         text = l.content
         if index == 0:
@@ -175,20 +175,23 @@ def generate_html_node_for_quoted_block(element):
         if not text:
             text = chr(0xA0)  # Non breaking space, so the div is forced to display.
         linediv.text = text
-    return container
+    yield container
 
 
 def generate_html_node_for_article(article, abbreviations):
     current_ref = article.relative_reference
-    container = ET.Element('div', {"id": current_ref.relative_id_string})
+    id_element = ET.Element('div', {"id": current_ref.relative_id_string, 'class': 'article_id'})
+    id_element.text = '{}. §'.format(article.identifier)
+    yield id_element
+
     if article.title:
-        title = ET.Element('div', {'class': 'article_title'})
-        title.text = '[{}]'.format(article.title)
-        container.append(title)
+        title_element = ET.Element('div', {'class': 'article_title'})
+        title_element.text = '[{}]'.format(article.title)
+        yield title_element
 
-    container.append(generate_html_node_for_sub_article_elements(article.children, current_ref, abbreviations))
+    yield from generate_html_nodes_for_children(article, current_ref, abbreviations)
 
-    return container
+    yield ET.Element('div', {'class': 'space_after_article'})
 
 
 def generate_html_body_for_act(act, indent=True):
@@ -203,13 +206,11 @@ def generate_html_body_for_act(act, indent=True):
     abbreviations = {}
     for c in act.children:
         if isinstance(c, Article):
-            id_string = '{}. §'.format(c.identifier)
-            content = generate_html_node_for_article(c, abbreviations)
+            elements_to_add = generate_html_node_for_article(c, abbreviations)
         else:
-            id_string = ''
-            content = generate_html_node_for_structural_element(c)
-        body_elements.append((id_string, content))
-    body.append(generate_html_list(body_elements, Article))
+            elements_to_add = generate_html_node_for_structural_element(c)
+        for element_to_add in elements_to_add:
+            body.append(element_to_add)
     if indent:
         indent_etree_element_in_place(body)
     return body
