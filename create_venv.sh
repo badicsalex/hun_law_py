ccd47c1ece8116a27605e1feab257dd6cff0f3ec
Alex Badics <admin@stickman.hu>
2020-10-15 12:02:05 +0200

    project: create pypy3 venv too with create_venv
    Also fixed requirements.txt for pypy: mypy and pypy are not compatible with
    each-other, so mypy can only be installed into a CPython venv.
    
    See: #14

--- create_venv.sh
+++ create_venv.sh
@@ -16,8 +16,18 @@
 # You should have received a copy of the GNU General Public License
 # along with Hun-Law.  If not, see <https://www.gnu.org/licenses/>.
 
-VENV_PATH=.venv
-python3 -m venv "${VENV_PATH}"
-"${VENV_PATH}/bin/pip" install --upgrade pip
-"${VENV_PATH}/bin/pip" install -r requirements.txt
+function create_venv(){
+    PYTHON_INTERPRETER="$1"
+    VENV_PATH="$2"
+    "${PYTHON_INTERPRETER}" -m venv "${VENV_PATH}"
+    "${VENV_PATH}/bin/pip" install --upgrade pip
+    "${VENV_PATH}/bin/pip" install -r requirements.txt
+}
+
+create_venv python3 .venv
+if ! command -v pypy3 &> /dev/null; then
+    echo "WARN: Pypy3 venv not installed. Consider installing pypy3 for 10x faster execution."
+else
+    create_venv pypy3 .venv-pypy
+fi
 
